278ef78ee6e68917b25f13de1d9586bacc216165
ChrisA <afarianchris@cityuniversity.edu>
2025-09-09 15:29:23 +0000

    source updated

--- src/pages/Project.ts
+++ src/pages/Project.ts
@@ -1,1 +1,14 @@
 import {initTicTacToe} from '../projects/tictactoe/tictactoe';
+export default function Projec(root: HTMLElement){
+    root.innerHTML = `
+    <h1>Projects</h1>
+    <p> Here's a few fun things we made.</p>
+    
+    <section>
+        <h2> Tic-tac-toe</h2>
+        <div id='tictactoe"></div>
+    </section>
+    `;
+    const mount = root.querySelector('#tictactoe') as HTMLElement;
+    initTicTacToe(mount);
+}
